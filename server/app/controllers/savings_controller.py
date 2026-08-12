26d2f178fdbcf4ef701bd3db28787b26a854a801
TimDev14 <njagi.timothy14@gmail.com>
2026-08-13 01:00:08 +0300

    feat: update SavingsController to use new saving_schema for serialization and improve response messages; add complete SourcesController with CRUD operations

--- server/app/controllers/savings_controller.py
+++ server/app/controllers/savings_controller.py
@@ -1,7 +1,7 @@
 from extensions import db
 from utils import get_current_user_id
 from models.savings import Savings
-from schemas.savings_schema import SavingsSchema
+from schemas.savings_schema import saving_schema, savings_schema
 
 from flask import jsonify
 
@@ -14,24 +14,24 @@ class SavingsController:
         saving = Savings(**payload)
         db.session.add(saving)
         db.session.commit()
-        return jsonify({"message": "New saving created successfully", "saving": SavingsSchema().dump(saving)}), 201
+        return jsonify({"message": "New saving created successfully", "saving": saving_schema().dump(saving)}), 201
     # Get one saving by ID
     @classmethod()
     def get_saving(cls, saving_id):
         if not saving_id:
             return jsonify({"message": "Saving not found"}), 400
         saving = Savings.query.filter_by(id=saving_id).first()
-        return jsonify({"saving": SavingsSchema().dump(saving)}), 200
+        return jsonify({"saving": saving_schema().dump(saving)}), 200
     # Get all savings
     @classmethod
     def get_all_savings(cls):
         savings = Savings.query.all()
-        return jsonify({"savings": SavingsSchema(many=True).dump(savings)}), 200
+        return jsonify({"savings": savings_schema(many=True).dump(savings)}), 200
     # Get all savings for one user
     @classmethod
     def get_user_savings(cls):
         savings = Savings.query.filter_by(user_id=cls.user).all()
-        return jsonify({"savings": SavingsSchema(many=True).dump(savings)}), 200
+        return jsonify({"savings": savings_schema.dump(savings)}), 200
     # update saving
     @classmethod
     def update_saving(cls, saving_id, data):
@@ -41,7 +41,7 @@ class SavingsController:
         for key, value in data.items():
             setattr(saving, key, value)
         db.session.commit()
-        return jsonify({"message": "Saving updated successfully", "saving": SavingsSchema().dump(saving)}), 200
+        return jsonify({"message": "Saving updated successfully", "saving": saving_schema().dump(saving)}), 200
     # delete saving
     @classmethod
     def delete_saving(cls, saving_id):
--- server/app/controllers/sources_controller.py
+++ server/app/controllers/sources_controller.py
@@ -1,14 +1,58 @@
 from extensions import db
-from utils import get_current_user_id, hash_password, verify_password, create_token
+from utils import get_current_user_id
 from models.sources import Sources
+from schemas.sources_schema import source_schema, sources_schema
 
-from flask import request, jsonify
+from flask import jsonify
 
 class SourcesController:
+    user = get_current_user_id()
     # Create a new source
+    @classmethod
+    def create_new_source(cls, data):
+        payload = dict(data)
+        new_source = Sources(**payload)
+        db.session.add(new_source)
+        db.session.commit()
+        return jsonify({"message": "Source successfully created", "source": source_schema.dump(new_source)}), 201
     # Get one source by ID
-    # Get all sources
-    # update source
-    # delete source
+    @classmethod
+    def get_source(cls, source_id):
+        source = Sources.query.filter_by(id=source_id).first()
+        if not source:
+            return jsonify({"message": "Source not found"}), 204
+        return jsonify({"source": source_schema.dump(source)}), 200
 
-    pass
+    # Get all sources
+    @classmethod
+    def get_all_sources(cls):
+        all_sources = Sources.query.all()
+        if not all_sources:
+            return jsonify({"message": "No source found"}), 204
+        return jsonify({"sources": sources_schema.dump(all_sources)}), 200
+    # Get all sources for a user
+    @classmethod
+    def get_sources_for_id(cls):
+        all_sources = Sources.query.filter_by(user_id=cls.user).all()
+        if not all_sources:
+            return jsonify({"message": "Sources not found for user"})
+        return jsonify({"sources": sources_schema.dump(all_sources)}), 200
+    # update source
+    @classmethod
+    def update_source(cls, data, source_id):
+        source = Sources.query.filter_by(id=source_id).first()
+        if not source:
+            return jsonify({"message": "Source not found"}), 404
+        for key, value in data.items():
+            setattr(source, key, value)
+            db.session.commit()
+        return jsonify({"message": "Source updated successfully", "source": source_schema.dump(source)}), 200
+    # delete source
+    @classmethod
+    def delete_source(cls, source_id):
+        source = Sources.query.filter_by(id=source_id).first()
+        if not source:
+            return jsonify({"message": "Source not found"}), 404
+        db.session.delete(source)
+        db.session.commit()
+        return jsonify({"message": "Source deleted successfully"}), 200
